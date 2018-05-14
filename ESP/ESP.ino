4dcd8a391a22af0d585da39cdbea32ef4d481183
Rodolfo Schiavi <rodolfocmschiavi@gmail.com>
2018-05-13 23:46:52 -0300

    correções no código da ESP

--- ESP/ESP.ino
+++ ESP/ESP.ino
@@ -1,3 +1,6 @@
+//Programa para comunicação Websocket entre um PC e uma ESP8266 
+//para enviar comandos via serial para um arduino com grbl
+
 #include <ESP8266WebServer.h>
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
@@ -6,9 +9,9 @@
 
 WebSocketsServer webSocket = WebSocketsServer(80); //Ativa a comunicação por websocket na porta 80
 
+//Atribuições para conexão com o roteador
 const char* ssid     = "SSID";
 const char* password = "PASSWORD";
-uint8_t endereco=0;
 
 
 void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t lenght) {
@@ -16,13 +19,12 @@ void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t lenght
   switch (type) { //verifica o tipo de mensagem recebida
     case WStype_CONNECTED: { //caso conecte
         IPAddress ip = webSocket.remoteIP(num);
-        endereco=num;
-        webSocket.sendTXT(num, "Conectado");
+        webSocket.sendTXT(num, "Conectado"); //Envia a mensagem para o cliente
       }
       break;
-    case WStype_TEXT: {
-        String text = String((char *) &payload[0]);
-        Serial.println(text);
+    case WStype_TEXT: { //Caso receba mensagem
+        String text = String((char *) &payload[0]); //Converte a mensagem para string
+        Serial.println(text); //Envia o comando via serial para o arduino
         break;
       }
 
@@ -31,13 +33,10 @@ void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t lenght
 
 void setup() {
   Serial.begin(115200);
-  MeuServo.attach(0); //Declarando Servo
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) { //Conectando a rede
-    Serial.println(".");
     delay(100);
   }
-  Serial.println(WiFi.localIP()); //Mostra o IP na rede
   webSocket.begin();
   webSocket.onEvent(webSocketEvent);
 }
